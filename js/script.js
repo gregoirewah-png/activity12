989fb593b1e93d131c128b91e8687fba629ff6da
Santiago Gregoire <gregoirewah@gmail.com>
2026-04-19 00:40:36 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            chuck: [ 
+                { "value": "Chuck Norris can skydive into outer space." }, 
+                { "value": "The chief export of Chuck Norris is pain." }, 
+                { "value": "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." }, 
+                { "value": "Time waits for no man. Unless that man is Chuck Norris." }, 
+                { "value": "If you spell Chuck Norris in Scrabble, you win. Forever." } 
+            ]
+        }
+    }
+}).mount('#app');
